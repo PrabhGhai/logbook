8d26d5d7ed7bb2d36e17b59e409b2d5551bb04ca
The Code Master YT <developerfullstack2000@gmail.com>
2024-06-24 16:49:43 +0530

    second commit

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ require("./conn/conn");
 app.use(express.json());
 
 //body parser
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://inscribelog.netlify.app/"],
+    credentials: true,
+  })
+);
 
 //cookie parser
 app.use(cookieParser());
